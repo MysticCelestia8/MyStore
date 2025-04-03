15c7185903d8b4e53af8b021127dcb2f435b45ff
Purbali <purbalighosh8@gmail.com>
2025-04-03 23:31:11 +0530

    Initial Commit

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,3 +1,47 @@
+const products = [  
+    {  
+      id: 1,  
+      product_name: "Apple",  
+      price: 150,  
+      image: "Apples.jpg"  
+    },  
+    {  
+      id: 2,  
+      product_name: "Pineapple",  
+      price: 75,  
+      image: "Pineapple.jpg"  
+    },  
+    {  
+      id: 3,  
+      product_name: "Orange",  
+      price: 120,  
+      image: "Orange.jpg"  
+    },  
+    {  
+      id: 4,  
+      product_name: "Dragon Fruit",  
+      price: 450,  
+      image: "Dragon Fruit.jpg"  
+    },  
+    {  
+      id: 5,  
+      product_name: "Strawberry",  
+      price: 300,  
+      image: "Strawberry.jpg"  
+    }  
+  ];  
+
 exports.renderProducts = (req,res)=>{
-    res.render("home");
+    res.render("home" ,{products:products});
+}
+exports.renderAddProduct = (req,res)=>{
+    res.render("addProduct")
+}
+exports.renderEditProduct = (req,res)=>{
+    res.render(
+      "editProduct",
+      {
+        product : products[--req.params.id]
+      }
+    )
 }
--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
 const home = require('./routes/home')  
+const addProduct = require('./routes/addProduct')  
+const editProduct = require('./routes/editProduct')  
 
 app.use(express.static('public'))
 
 app.set('view engine','ejs');
 app.set('views','views')
 
+
 app.use('/',home)
+app.use('/addProduct',addProduct)
+app.use('/editProduct',editProduct)
 const server = app.listen(3000, ()=>{
      console.log("Server Running...");
 })
